897dee60c8ebea7bc2108974ee45a7937d77d620
iliachel <i.chelevich@gmail.com>
2025-12-27 00:40:25 +0300

    trying to make just console chess messenger

--- client/main.cpp
+++ client/main.cpp
@@ -1,41 +1,51 @@
 #include <QCoreApplication>
 #include <QTcpSocket>
+#include <QSocketNotifier>
 #include <QTextStream>
 #include <QDebug>
+
+class ChessClient : public QObject {
+    Q_OBJECT
+public:
+    ChessClient(QString host, quint16 port) {
+        connect(&socket, &QTcpSocket::connected, this, [&](){
+            qDebug() << "Connected to server";
+        });
+
+        connect(&socket, &QTcpSocket::readyRead, this, [&](){
+            QByteArray data = socket.readAll();
+            qDebug() << "Server:" << data;
+        });
+
+        // stdin notifier
+        notifier = new QSocketNotifier(fileno(stdin), QSocketNotifier::Read, this);
+        connect(notifier, &QSocketNotifier::activated, this, &ChessClient::readUserInput);
+
+        socket.connectToHost(host, port);
+    }
+
+private slots:
+    void readUserInput() {
+        QTextStream in(stdin);
+        QString line = in.readLine();
+
+        if(line.isEmpty())
+            return;
+
+        socket.write(line.toUtf8() + "\n");
+        socket.flush();
+    }
+
+private:
+    QTcpSocket socket;
+    QSocketNotifier *notifier;
+};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QTcpSocket socket;
-
-    socket.connectToHost("127.0.0.1", 5555);
-    if(!socket.waitForConnected(3000)) {
-        qDebug() << "Connection failed";
-        return 0;
-    }
-
-    qDebug() << "Connected to server";
-
-    QObject::connect(&socket, &QTcpSocket::readyRead, [&](){
-        QByteArray data = socket.readAll();
-        qDebug() << "Server:" << data;
-    });
-
-    QTextStream input(stdin);
-    QObject::connect(&a, &QCoreApplication::aboutToQuit, [&](){
-        socket.close();
-    });
-
-    // простой ввод ходов
-    QObject::connect(&socket, &QTcpSocket::connected, [&](){
-        qDebug() << "Enter moves like e2e4";
-
-        while(true){
-            QString move = input.readLine();
-            socket.write(move.toUtf8() + "\n");
-            socket.flush();
-        }
-    });
-
+    ChessClient client("127.0.0.1", 5555);
     return a.exec();
 }
+
+#include "main.moc"
